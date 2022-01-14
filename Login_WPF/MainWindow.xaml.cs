d16b2c1a4bdd3762e60dcc40c4e9ca370eb1131a
joncavalcanti <92554844+joncavalcanti@users.noreply.github.com>
2022-01-14 01:05:23 -0300

    Update 0.1

---
+++ Login_WPF/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+using System.Data.SqlClient;
+
+namespace Login_WPF
+{
+   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        public string conString = "Data Source=DESKTOP-C45AQE3;Initial Catalog=db_users;Integrated Security=True";
+
+      
+        public bool isDarkTheme { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+        private void toggleTheme(object sender, RoutedEventArgs e)
+        {
+            ITheme theme = paletteHelper.GetTheme();
+
+            if (isDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                isDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+            }
+            else
+            {
+                isDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark);
+
+            }
+            paletteHelper.SetTheme(theme);
+        }
+
+        private void exitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
+        private void isClick(object sender, RoutedEventArgs e)
+        {
+            string password = "";
+            bool IsExist = false;
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            string login = "SELECT * FROM dbo.Account WHERE ID='"+txtUserName.Text+"'";
+            SqlCommand cmd = new SqlCommand(login,con);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+
+            if (dr.Read() == true)
+            {
+                password = dr.GetString(1);   
+                IsExist = true;
+            
+            }
+            con.Close();
+
+            if (IsExist)  
+            {
+                if (Cryptography.DecryptKey(password).Equals(txtPassword.Password.ToString()))
+                {
+                    new Dashboard().Show();
+                    this.Hide();
+                } 
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username or Password, Please Try Again", "Login Failed", MessageBoxButton.OK);
+                txtUserName.Text = "";
+                txtPassword.Password = null;
+                txtUserName.Focus();
+            }
+        }
+
+        private void isCreateAccount(object sender, RoutedEventArgs e)
+        {
+            new Register().Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Login_WPF/Register.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Login_WPF
+{
+    /// <summary>
+    /// Lógica interna para Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        public string conString = "Data Source=DESKTOP-C45AQE3;Initial Catalog=db_users;Integrated Security=True";
+
+        public bool isDarkTheme { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+      
+        private void toggleTheme(object sender, RoutedEventArgs e)
+        {
+            ITheme theme = paletteHelper.GetTheme();
+
+            if (isDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                isDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+            }
+            else
+            {
+                isDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark);
+
+            }
+            paletteHelper.SetTheme(theme);
+        }
+
+        private void exitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
+
+        private void isClickRegister(object sender, RoutedEventArgs e)
+        {
+            if (txtUserName.Text != "" && txtPassword.Password.ToString() != "")    
+            {
+               
+                    SqlConnection con = new SqlConnection(conString);
+                    con.Open();
+                    string register = "insert into dbo.Account(ID, Password) values('" + txtUserName.Text.ToString() + "' , '" + Cryptography.EncryptKey(txtPassword.Password.ToString()) + "')";
+                    SqlCommand cmd = new SqlCommand(register, con);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("You account is registered", "Success", MessageBoxButton.OK);
+                    new MainWindow().Show();
+                    this.Hide();
+                }
+            else  {
+                MessageBox.Show("Please fill all the fields!..", "Error", MessageBoxButton.OK); 
+            }
+        }
+
+            private void backToLogin(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            this.Hide();
+        }
+
+    }
+}
